255f5aec34a8089e8ffae479b917965948dea34b
Brandon Liu <bdon@bdon.org>
2014-07-22 14:31:56 -0700

    add imagemagick dependency

--- provisioning/install-dependencies.sh
+++ provisioning/install-dependencies.sh
@@ -3,7 +3,7 @@ set -e
 sudo apt-add-repository -y ppa:chris-lea/node.js
 sudo apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
-sudo apt-get install -y nodejs make mapnik-utils git unzip gdal-bin zip python-pip python-dev python-numpy python-scipy
+sudo apt-get install -y nodejs make mapnik-utils git unzip gdal-bin zip python-pip python-dev python-numpy python-scipy imagemagick
 cd ~/
 npm install carto millstone interp
 
