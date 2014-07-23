8ca2e26e1ea76f80e0e5ff323b002e2c84e3b4ce
Seth Fitzsimmons <seth@mojodna.net>
2014-07-23 14:43:04 -0700

    tilestache and modestmaps

--- provisioning/install-dependencies.sh
+++ provisioning/install-dependencies.sh
@@ -4,7 +4,8 @@ set -e
 sudo apt-add-repository -y ppa:chris-lea/node.js
 sudo apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
-sudo apt-get install -y nodejs make mapnik-utils git unzip gdal-bin zip python-pip python-dev python-numpy python-scipy imagemagick
+sudo apt-get install -y nodejs make mapnik-utils git unzip gdal-bin zip python-pip python-dev python-numpy python-scipy imagemagick python-modestmaps
+sudo pip install tilestache
 cd ~/
 npm install carto millstone interp
 
