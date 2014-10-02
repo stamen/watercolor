0a41c7961c00d40f818bea0334e716b021f13195
Brandon Liu <bdon@bdon.org>
2014-10-02 15:25:32 -0700

    version lock npm packages, make calls js-yaml

--- provisioning/install-dependencies.sh
+++ provisioning/install-dependencies.sh
@@ -7,6 +7,5 @@ sudo apt-get update
 sudo apt-get install -y nodejs make mapnik-utils git unzip gdal-bin zip python-pip python-dev python-numpy python-scipy imagemagick python-modestmaps
 sudo pip install tilestache nik4
 cd ~/
-npm install carto millstone interp
-
+npm install carto millstone interp@0.1.0 js-yaml@3.2.1
 
